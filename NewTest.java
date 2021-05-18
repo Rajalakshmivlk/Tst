c8d301cd070f057f336103573e6e67c2d2f09539
Rajalakshmivlk <77186047+Rajalakshmivlk@users.noreply.github.com>
2021-05-18 21:39:41 +0530

    Add files via upload

---
+++ NewTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void exampleOfTestNgMaven() {
+      System.out.println("This is TestNG-Maven Example");
+  }
+}
